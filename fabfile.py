27b47d7c56f11a5ba95d10373a54b02ca680a1c8
toki <toki.kanno@gmail.com>
2013-04-19 14:21:15 +0800

    add quiet mode to apt-get

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,9 @@
+from fabric.operations import put, sudo, get, local
+
 def install_fabric():
 	"""
 	This function install fabric on Ubuntu OS
 	"""
-	sudo('apt-get -y install python-pip gcc python-dev autoconf')
+	sudo('apt-get -y -q install python-pip gcc python-dev autoconf')
 	sudo('pip install fabric')
 
